cdc64ccd8fb3d6778cd4090c1bb19fa52869cf8f
Christian Gonzalez <chrisgo@bu.edu>
2026-05-16 12:23:25 -0500

    oauth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "./globals.css";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
+import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <ConvexAuthNextjsServerProvider>
-      <html lang="en">
+      <html lang="en" className={cn("font-sans", figtree.variable)}>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
